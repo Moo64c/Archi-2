d0f4ea2d301763f5a2d0bcc616c728daccd1caa0
rotemmia <rotemmia@post.bgu.ac.il>
2018-04-17 13:33:31 +0300

    calculating things

--- main.c
+++ main.c
@@ -7,11 +7,16 @@
 #include <math.h>
 
 double * complex_power(double * complex, int power);
+double * multiply_complex(double * complex1, double * complex2);
+double * apply_function(double coefficient[][2], int order, double * value);
+
+int DOUBLE_SIZE = 2;
 
 int main(void) {
     // Specific input for this program.
     double epsilon = 0;
     int order = 0;
+    DOUBLE_SIZE = (int) sizeof(double);
     // To be allocated later.
 
     // Tolerance.
@@ -21,39 +26,29 @@ int main(void) {
     //scanf("order = %i\n", &order);
 
     // COEFFICIENTS
-    // Allocate data and reset values.
-    double coefficients[order + 1][2];
-    for (int initIndex = 0; initIndex < order + 1; initIndex++) {
-        coefficients[initIndex][0] = 0.0;
-        coefficients[initIndex][1] = 0.0;
-    }
-
-    char acceptString[200];
-    for (int coefficientindex = order; coefficientindex >= 0; coefficientindex--) {
-        //sprintf(acceptString, "coeff %d = %s %s", coefficientindex, "%lf", "%lf");
-        //scanf(acceptString, &coefficients[coefficientindex][0], &coefficients[coefficientindex][1]);
-        // Catch "\n".
-        //getchar();
-    }
-    printf("\nResult:\n");
-    for (int printIndex = 0; printIndex <= order; printIndex++) {
-        printf("(%lf + %lf) * x ^ %d ", coefficients[printIndex][0], coefficients[printIndex][1], printIndex);
-    }
-
-    printf("\n");
+//    // Allocate data and reset values.
+//    double coefficients[order + 1][2];
+//    for (int initIndex = 0; initIndex < order + 1; initIndex++) {
+//        coefficients[initIndex][0] = 0.0;
+//        coefficients[initIndex][1] = 0.0;
+//    }
+//
+//    char acceptString[200];
+//    for (int coefficientindex = order; coefficientindex >= 0; coefficientindex--) {
+//        //sprintf(acceptString, "coeff %d = %s %s", coefficientindex, "%lf", "%lf");
+//        //scanf(acceptString, &coefficients[coefficientindex][0], &coefficients[coefficientindex][1]);
+//        // Catch "\n".
+//        //getchar();
+//    }
+//    printf("\nResult:\n");
+//    for (int printIndex = 0; printIndex <= order; printIndex++) {
+//        printf("(%lf + %lf) * x ^ %d ", coefficients[printIndex][0], coefficients[printIndex][1], printIndex);
+//    }
+//
+//    printf("\n");
 
     // INITIAL
 
-    double initial[2];
-    initial[0] = 5.0;
-    initial[1] = 2.0;
-    int power = 3;
-    double * result;
-    result = complex_power(initial, power);
-
-
-    printf("result: %lf + i%lf", result[0], result[1]);
-    free(result);
     //sprintf(acceptString, "initial = %s %s", "%lf", "%lf");
     //scanf(acceptString, &initial[0], &initial[1]);
     // Catch "\n".
@@ -63,9 +58,21 @@ int main(void) {
     printf("oeder : %d\n", order);
     printf("initial real : %lf\n", initial[0][0]);
     printf("initial imaginary : %lf\n", initial[0][1]);
-*/
+//*/
+    double coeff[3][2];
+    coeff[0][0] = 0.0;
+    coeff[0][1] = 0.0;
+    coeff[1][0] = 1.0;
+    coeff[1][1] = 0.0;
+    coeff[2][0] = 0.0;
+    coeff[2][1] = 0.0;
 
+    double value[2];
+    value[0] = 4.0;
+    value[1] = 1.0;
 
+    double * result = apply_function(coeff, 2, value);
+    printf("result: %lf + %lfi", result[0], result[1]);
 
 
     return 0;
@@ -74,7 +81,7 @@ int main(void) {
 /**
  * Gets the result of applying a polynomial function with a specific value.
  */
-double * apply_function(double ** coefficient, int order, double * value) {
+double * apply_function(double coefficient[][2], int order, double * value) {
     //
     double * result = (double *) malloc(2 * sizeof(double));
     result[0] = 0.0;
@@ -82,23 +89,40 @@ double * apply_function(double ** coefficient, int order, double * value) {
     for (int index = 0; index <= order; index++) {
         // Calculate value ^ (current order) * coefficient.
         double * step = complex_power(value, index);
+        fprintf(stderr, "foo %d\n", index);
 
-        // FIXME (this is wrong).
-        step[0] = step[0] * coefficient[index][0];
-        step[1] = step[1] * coefficient[index][1];
+        step = multiply_complex(step, coefficient[index]) ;
+        fprintf(stderr, "foo2 %d\n", index);
         // A
         result[0] += step[0];
         result[1] += step[1];
+
     }
 
 
-    return 0;
+    return result;
+}
+
+double * multiply_complex(double * complex1, double * complex2) {
+    double * result = (double *) malloc(2 * sizeof(double));
+    fprintf(stderr, "\n%lf %lf\n", complex1[0], complex2[0]);
+    result[0] = complex1[0] * complex2[0] - complex1[1] * complex2[1];
+    result[1] = complex1[0] * complex2[1] + complex1[1] * complex2[0];
+    return result;
 }
 
 /**
  * Calculates raising a complex number to the power specified.
  */
 double * complex_power(double * complex, int power) {
+    double * result = (double *) malloc(2 * sizeof(double));
+
+    if (power == 0) {
+        result[0] = 1.0;
+        result[1] = 0.0;
+        return result;
+    }
+
     // Base values.
     double radius = sqrt(complex[0] * complex[0] + complex[1] * complex[1]);
     double thetha = atan(complex[1] / complex[0]);
@@ -106,8 +130,7 @@ double * complex_power(double * complex, int power) {
     radius = powf(radius, power);
     thetha = thetha * power;
 
-    // Convert back to standard representation.
-    double * result = (double *) malloc(2 * sizeof(double));
+    // Convert back to standard representation.;
     result[0] = radius * (cos(thetha));
     result[1] = radius * (sin(thetha));
     return result;
