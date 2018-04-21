9a0ca58b0a8531d200912743917e6e65a558a8d0
rotemmia <rotemmia@post.bgu.ac.il>
2018-04-21 21:00:47 +0300

    almost there now

--- asm.s
+++ asm.s
@@ -1,1 +1,89 @@
+calculate_derivative:
+; Generic start
+push rbp
+mov rbp, rsp
 
+push rbx
+; Increase stack size.
+sub rsp, 40
+
+mov [rbp-40], rdi               ; coefficients
+mov [rbp-44], esi               ; order
+mov	eax, [rbp-44]
+sal	rax, 3
+mov	rdi, rax
+call	malloc
+mov [rbp-24], rax               ; derivative
+
+; loop start with index=1
+mov	DWORD [rbp-28], 1           ; index
+jmp	.L1
+
+.L2:
+
+;
+  mov	eax, [rbp-28]
+  sal	rax, 3
+  lea	rdx, [rax-8]
+  mov	rax, [rbp-24]
+  lea	rbx, [rdx+rax]           ; placed in index - 1
+  mov	edi, 16                  ; sizeof double * 2
+  call	malloc
+  mov [rbx], rax
+
+  mov	eax, [rbp-28]           ; index
+  cdqe
+  sal	rax, 3
+  lea	rdx, [rax-8]
+  mov	rax, [rbp-24]
+  add	rax, rdx
+  mov	rax, [rax]
+  pxor	xmm0, xmm0
+  cvtsi2sd	xmm0, [rbp-28]
+  mov	edx, [rbp-28]
+  movsx	rdx, edx
+  lea	rcx, [0+rdx*8]
+  mov	rdx, [rbp-40]
+  add	rdx, rcx
+  mov	rdx, [rdx]
+  movsd	xmm1, [rdx]
+  mulsd	xmm0, xmm1
+  movsd	[rax], xmm0
+  mov	eax, [rbp-28]
+  cdqe
+  sal	rax, 3
+  lea	rdx, [rax-8]
+  mov	rax, [rbp-24]
+  add	rax, rdx
+  mov	rax, [rax]
+  add	rax, 8
+  pxor	xmm0, xmm0
+  cvtsi2sd	xmm0,  [rbp-28]
+  mov	edx,  [rbp-28]
+  movsx	rdx, edx
+  lea	rcx, [0+rdx*8]
+  mov	rdx, [rbp-40]
+  add	rdx, rcx
+  mov	rdx, [rdx]
+  add	rdx, 8
+  movsd	xmm1, [rdx]
+  mulsd	xmm0, xmm1
+  movsd [rax], xmm0
+  add	DWORD [rbp-28], 1
+
+.L1:
+; oder must be bigger \ eaqual to index.
+; if so, move to .L2
+  mov	eax,  [rbp-28]
+  cmp	eax,  [rbp-44]
+  jle	.L1
+
+; return derivative
+  mov	rax, [rbp-24]
+  add	rsp, 40
+
+  ; Pop used registers and return.
+  pop	rbx
+  pop	rbp
+
+  ret
--- assembly.s
+++ assembly.s
@@ -547,90 +547,79 @@ sub rsp, 32
     ret
 
     calculate_derivative:
-    ; Generic start
-    push rbp
-    mov rbp, rsp
+    .LFB5:
 
-    push rbx
-    ; Increase stack size.
-    sub rsp, 40
-
-    mov [rbp-40], rdi               ; coefficients
-    mov [rbp-44], esi               ; order
-    mov	eax, [rbp-44]
-    sal	rax, 3
-    mov	rdi, rax
-    call	malloc
-    mov [rbp-24], rax               ; derivative
-
-    ; loop start with index=1
-    mov	DWORD [rbp-28], 1           ; index
-    jmp	.L1
-
-    .L2:
-
-    ;
-    	mov	eax, [rbp-28]
+    	push	rbp
+    	mov	rbp, rsp
+    	push	rbx
+    	sub	rsp, 40
+    	mov	QWORD [rbp-40], rdi
+    	mov	DWORD [rbp-44], esi
+    	mov	eax, DWORD [rbp-44]
+    	cdqe
     	sal	rax, 3
-    	lea	rdx, [rax-8]
-    	mov	rax, [rbp-24]
-    	lea	rbx, [rdx+rax]           ; placed in index - 1
-    	mov	edi, 16                  ; sizeof double * 2
+    	mov	rdi, rax
     	call	malloc
-    	mov [rbx], rax
-
-    	mov	eax, [rbp-28]
-    	sal	rax, 3
-    	lea	rdx, [rax-8]
-    	mov	rax, [rbp-24]
-    	add	rax, rdx
-    	mov	rax, [rax]
-    	pxor	xmm0, xmm0
-    	cvtsi2sd	xmm0, [rbp-28]
-    	mov	edx, [rbp-28]
-    	movsx	rdx, edx
-    	lea	rcx, [0+rdx*8]
-    	mov	rdx, [rbp-40]
-    	add	rdx, rcx
-    	mov	rdx, [rdx]
-    	movsd	xmm1, [rdx]
-    	mulsd	xmm0, xmm1
-    	movsd	[rax], xmm0
-    	mov	eax, [rbp-28]
+    	mov	QWORD [rbp-24], rax
+    .LBB5:
+    	mov	DWORD [rbp-28], 1
+    	jmp	.L18
+    .L19:
+    	mov	eax, DWORD [rbp-28]
     	cdqe
     	sal	rax, 3
     	lea	rdx, [rax-8]
-    	mov	rax, [rbp-24]
+    	mov	rax, QWORD [rbp-24]
+    	lea	rbx, [rdx+rax]
+    	mov	edi, 16
+    	call	malloc
+    	mov	QWORD [rbx], rax
+    	mov	eax, DWORD [rbp-28]
+    	cdqe
+    	sal	rax, 3
+    	lea	rdx, [rax-8]
+    	mov	rax, QWORD [rbp-24]
     	add	rax, rdx
-    	mov	rax, [rax]
-    	add	rax, 8
+    	mov	rax, QWORD [rax]
     	pxor	xmm0, xmm0
-    	cvtsi2sd	xmm0,  [rbp-28]
-    	mov	edx,  [rbp-28]
+    	cvtsi2sd	xmm0, DWORD [rbp-28]
+    	mov	edx, DWORD [rbp-28]
     	movsx	rdx, edx
     	lea	rcx, [0+rdx*8]
-    	mov	rdx, [rbp-40]
+    	mov	rdx, QWORD [rbp-40]
     	add	rdx, rcx
-    	mov	rdx, [rdx]
-    	add	rdx, 8
-    	movsd	xmm1, [rdx]
+    	mov	rdx, QWORD [rdx]
+    	movsd	xmm1, QWORD [rdx]
     	mulsd	xmm0, xmm1
-    	movsd [rax], xmm0
+    	movsd	QWORD [rax], xmm0
+    	mov	eax, DWORD [rbp-28]
+    	cdqe
+    	sal	rax, 3
+    	lea	rdx, [rax-8]
+    	mov	rax, QWORD [rbp-24]
+    	add	rax, rdx
+    	mov	rax, QWORD [rax]
+    	add	rax, 8
+    	pxor	xmm0, xmm0
+    	cvtsi2sd	xmm0, DWORD [rbp-28]
+    	mov	edx, DWORD [rbp-28]
+    	movsx	rdx, edx
+    	lea	rcx, [0+rdx*8]
+    	mov	rdx, QWORD [rbp-40]
+    	add	rdx, rcx
+    	mov	rdx, QWORD [rdx]
+    	add	rdx, 8
+    	movsd	xmm1, QWORD [rdx]
+    	mulsd	xmm0, xmm1
+    	movsd	QWORD [rax], xmm0
     	add	DWORD [rbp-28], 1
-
-    .L1:
-    ; oder must be bigger \ eaqual to index.
-    ; if so, move to .L2
-    	mov	eax,  [rbp-28]
-    	cmp	eax,  [rbp-44]
-    	jle	.L1
-
-    ; return derivative
-    	mov	rax, [rbp-24]
+    .L18:
+    	mov	eax, DWORD [rbp-28]
+    	cmp	eax, DWORD [rbp-44]
+    	jle	.L19
+    .LBE5:
+    	mov	rax, QWORD [rbp-24]
     	add	rsp, 40
-
-      ; Pop used registers and return.
     	pop	rbx
     	pop	rbp
-
     	ret
